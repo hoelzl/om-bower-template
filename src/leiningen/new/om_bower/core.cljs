1d3807ed821a63b4537ac386b740e3881ae710a6
Matthias Hoelzl <tc@xantira.com>
2014-08-27 06:43:14 +0200

    Add imports for bootstrap and tools to core

--- src/leiningen/new/om_bower/core.cljs
+++ src/leiningen/new/om_bower/core.cljs
@@ -1,11 +1,23 @@
 (ns {{namespace}}
-  (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [cljs.core.async :refer [put! chan <!]]
+            [om.core :as om :include-macros true]
+            ;; [om.dom :as dom :include-macros true]
+            [om-bootstrap.button :as btn]
+            [om-bootstrap.grid :as grid]
+            [om-bootstrap.input :as inp]
+            [om-bootstrap.mixins :as mix]
+            [om-bootstrap.nav :as nav]
+            [om-bootstrap.panel :as panel]
+            [om-bootstrap.progress-bar :as pb]
+            [om-bootstrap.random :as rand]
+            [om-bootstrap.table :refer [table]]
+            [om-tools.core :refer-macros [defcomponent defcomponentk]]
+            [om-tools.dom :as dom :include-macros true]))
 
 (enable-console-print!)
 
 (def app-state (atom {:text "Hello world!"}))
-
 
 (om/root
   (fn [app owner]
