fbd1d953742a2e6a7878967cb8538e80f922967e
Matthias Hoelzl <tc@xantira.com>
2014-08-28 19:54:08 +0200

    Update dependencies, improve template

--- src/leiningen/new/om_bower/core.cljs
+++ src/leiningen/new/om_bower/core.cljs
@@ -1,28 +1,65 @@
 (ns {{namespace}}
-  (:require-macros [cljs.core.async.macros :refer [go]])
-  (:require [cljs.core.async :refer [put! chan <!]]
+  (:require-macros [cljs.core.async.macros :refer [go]]
+                   [plumbing.core :refer [for-map lazy-get
+                                          ?>> ?> fn-> fn->> <- as->>
+                                          memoized-fn
+                                          letk fnk defnk]])
+  (:require [cljs.core.async :refer [chan <! >! put! take!]]
             [om.core :as om :include-macros true]
+            ;; Comment-out the line [om-tools.dom :as dom ...] and
+            ;; comment-in this line to use the vanilla version of
+            ;; om.dom instad of the Prismatic one.
             ;; [om.dom :as dom :include-macros true]
-            [om-bootstrap.button :as btn]
+
+            [om-bootstrap.button :as button]
             [om-bootstrap.grid :as grid]
-            [om-bootstrap.input :as inp]
-            [om-bootstrap.mixins :as mix]
+            [om-bootstrap.input :as input]
+            [om-bootstrap.mixins :as mixins]
             [om-bootstrap.nav :as nav]
             [om-bootstrap.panel :as panel]
-            [om-bootstrap.progress-bar :as pb]
-            [om-bootstrap.random :as rand]
+            [om-bootstrap.progress-bar :as progress-bar]
+            [om-bootstrap.random :as random]
             [om-bootstrap.table :refer [table]]
+
             [om-tools.core :refer-macros [defcomponent defcomponentk]]
-            [om-tools.dom :as dom :include-macros true]))
+            [om-tools.dom :as dom :include-macros true]
+            [plumbing.core :refer [update map-vals map-keys map-from-keys
+                                   map-from-vals dissoc-in keywordize-map
+                                   safe-get safe-get-in
+                                   assoc-when update-in-when grouped-map
+                                   aconcat unchunk sum singleton
+                                   indexed positions distinct-by
+                                   interleave-all count-when conj-when
+                                   cons-when rsort-by
+                                   swap-pair! get-and-set! millis
+                                   mapply]]))
 
 (enable-console-print!)
 
-(def app-state (atom {:text "Hello world!"}))
+(def app-state (atom {:heading "Hello world!"
+                      :text-1 "This is a template using Om together
+                      with Twitter Bootstrap and some other
+                      components.  This text is in a column that
+                      should take up the full width of the screen on
+                      phones or if you have a narrow browser window,
+                      and occupy the left column of a two-column
+                      layout on other devices."
+                      :text-2 "This text is in a column that should
+                      take up the full width of the screen on phones
+                      or if you have a narrow browser window, and
+                      occupy the right column of a two-column layout
+                      on other devices."}))
 
-(om/root
-  (fn [app owner]
-    (reify om/IRender
-      (render [_]
-        (dom/h1 nil (:text app)))))
-  app-state
-  {:target (. js/document (getElementById "app"))})
+(defcomponent hello [data owner]
+  (render [_]
+    (dom/div {:class "container-fluid"}
+      (dom/h1 (:heading data))
+      (dom/div {:class "row"}
+        (dom/div {:class "col-xs-12 col-md-6"}
+          (dom/p (:text-1 data)))
+        (dom/div {:class "col-xs-12 col-md-6"}
+          (dom/p (:text-2 data)))))))
+
+(om/root hello
+         app-state
+         {:target (. js/document (getElementById "app"))})
--- src/leiningen/new/om_bower/project.clj
+++ src/leiningen/new/om_bower/project.clj
@@ -13,11 +13,12 @@
   :source-paths ["src/clj" "src/cljs"]
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2311"]
+                 [org.clojure/clojurescript "0.0-2322"]
                  [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
-                 [om "0.7.1"]
                  [com.facebook/react "0.11.1"]
+                 [om "0.7.1"]
                  [prismatic/om-tools "0.3.3"]
+                 [prismatic/plumbing "0.3.3"]
                  [racehub/om-bootstrap "0.2.6"]]
   
   :plugins [[lein-cljsbuild "1.0.3"]]
